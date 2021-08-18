bc381ffcc0547221172feb236ce72ef13645d66b
Garmode3073 <gauravgarmode16@gmail.com>
2021-08-18 07:49:29 +0530

    global set up and adding dependencies

--- lib/global.dart
+++ lib/global.dart
@@ -1,1 +1,22 @@
+//measurements
+import 'package:flutter/material.dart';
+
 double width, height;
+
+//color swatch
+Map<int, Color> color = {
+  50: Color.fromRGBO(152, 114, 255, .1),
+  100: Color.fromRGBO(152, 114, 255, .2),
+  200: Color.fromRGBO(152, 114, 255, .3),
+  300: Color.fromRGBO(152, 114, 255, .4),
+  400: Color.fromRGBO(152, 114, 255, .5),
+  500: Color.fromRGBO(152, 114, 255, .6),
+  600: Color.fromRGBO(152, 114, 255, .7),
+  700: Color.fromRGBO(152, 114, 255, .8),
+  800: Color.fromRGBO(152, 114, 255, .9),
+  900: Color.fromRGBO(152, 114, 255, 1),
+};
+
+//colors
+
+Color primary = MaterialColor(0xff9872FF, color);
