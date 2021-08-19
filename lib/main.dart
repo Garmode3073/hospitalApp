b299e58e6de9c9031912ffd9857c9aa1c51e9d9e
Garmode3073 <gauravgarmode16@gmail.com>
2021-08-19 16:06:28 +0530

    profile edit page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hospitalApp/global.dart' as g;
 import 'package:hospitalApp/screens/landing.dart';
+import 'package:hospitalApp/screens/profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return LandingPage();
+    return ProfilePage();
   }
 }
